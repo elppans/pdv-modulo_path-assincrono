af4590dd773b66d20af71e0d764b34e88ca89e2d
elppans <elppansmk@gmail.com>
2025-07-03 17:13:13 -0300

    Update atualizamodulo.sh

--- atualizamodulo/atualizamodulo.sh
+++ atualizamodulo/atualizamodulo.sh
@@ -10,6 +10,7 @@ DATE="$(date +%d%m%Y)"
 DOCKER_USER="usuario_docker"
 DOCKER_PASS="senha_docker"
 DIRMOD="/Zanthus/Zeus/pdvJava/GERAL/SINCRO/WEB/moduloPHPPDV"
+YML="$DIRMOD/docker-compose.yml"
 PDVIP="$(ifconfig eth0 | grep "inet " | awk '{print $2}')"
 
 cd /home/zanthus/
@@ -29,8 +30,36 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+if [ ! -f "$YML" ]; then
+echo -e '#*********MODULOPHPPDV********* 
+networks:
+  zanthus-network:
+    driver: bridge
+    external: true
+services:
+  moduloPHPPDV:
+    image: zanthusinovacao1/managerweb:moduloPHPPDV
+    restart: always
+    hostname: moduloPHPPDV
+    container_name: moduloPHPPDV
+    privileged: true
+    entrypoint: ./entrypoint.sh
+    ports:
+      - "9090:9090"
+    networks:
+      - zanthus-network
+    environment:
+      - TZ=America/Bahia
+    volumes:
+      - /Zanthus/Zeus/pdvJava/GERAL/SINCRO/WEB:/usr/local/php/moduloPHPPDV
+      - /Zanthus/Zeus/pdvJava/GERAL/SINCRO/WEB/moduloPHPPDV/php_errors.log:/tmp/php_errors.log
+' | sudo tee "$YML" &>>/dev/null
+fi
+
 echo "Atualizando os containers..."
 cd "$DIRMOD"
+>php_errors.log
+docker network inspect zanthus-network >/dev/null 2>&1 || \
 docker network create zanthus-network
 docker-compose up -d
 
